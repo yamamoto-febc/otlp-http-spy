c6cf84eceabe145d616951d43bf309291745512c
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2025-03-31 08:19:28 +0900

    Simplified structure by consolidating source files into main.go

--- main.go
+++ main.go
@@ -18,6 +18,26 @@ import (
 
 var config Config
 
+type Config struct {
+	ListenAddr      string `env:"LISTEN_ADDR" envDefault:":4318"`
+	Endpoint        string `env:"ENDPOINT"`
+	LogsEndpoint    string `env:"LOGS_ENDPOINT"`
+	TracesEndpoint  string `env:"TRACES_ENDPOINT"`
+	MetricsEndpoint string `env:"METRICS_ENDPOINT"`
+}
+
+func (c *Config) Init() {
+	if c.LogsEndpoint == "" && c.Endpoint != "" {
+		c.LogsEndpoint = c.Endpoint + "/v1/logs"
+	}
+	if c.TracesEndpoint == "" && c.Endpoint != "" {
+		c.TracesEndpoint = c.Endpoint + "/v1/traces"
+	}
+	if c.MetricsEndpoint == "" && c.Endpoint != "" {
+		c.MetricsEndpoint = c.Endpoint + "/v1/metrics"
+	}
+}
+
 type protoRequestResponse struct {
 	request  proto.Message
 	response proto.Message
